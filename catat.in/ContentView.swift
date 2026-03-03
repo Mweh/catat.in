6740d3ccea718d716e2be3ce7659e54a5aa0c0e9
Fahmi <fahmianton@gmail.com>
2026-03-03 09:41:23 +0700

    Initial Commit

---
+++ catat.in/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  catat.in
+//
+//  Created by Fahmi-MB3 on 03/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ catat.in/catat_inApp.swift
@@ -0,0 +1,17 @@
+//
+//  catat_inApp.swift
+//  catat.in
+//
+//  Created by Fahmi-MB3 on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct catat_inApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
